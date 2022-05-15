b514b980a07ef548371f53344f1182705fd29735
Raisa Molokova <psraisaih@yandex.ru>
2022-05-15 15:55:12 +0300

    add Order class

--- src/main/java/ru/services/praktikum/qascooter/model/CourierCredentials.java
+++ src/main/java/ru/services/praktikum/qascooter/model/CourierCredentials.java
@@ -1,20 +1,24 @@
 package ru.services.praktikum.qascooter.model;
-
-import lombok.AllArgsConstructor;
+import lombok.*;
 import org.apache.commons.lang3.RandomStringUtils;
 
+@Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class CourierCredentials {
 
     private String login;
     private String password;
 
-     public static CourierCredentials from(Courier courier) {
+    public static CourierCredentials from(Courier courier) {
         return new CourierCredentials(courier.getLogin(), courier.getPassword());
     }
+
     public static CourierCredentials onlyLogin(){
         return new CourierCredentials(RandomStringUtils.randomAlphabetic(10),null);
+
     }
+
     public static CourierCredentials onlyPassword(){
         return new CourierCredentials(null, RandomStringUtils.randomAlphabetic(10));
     }
